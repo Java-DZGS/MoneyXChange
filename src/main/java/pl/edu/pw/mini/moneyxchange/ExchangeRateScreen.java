56090a7069f217eddfcac98cad4e60276ab4a40a
KuZu <jakubzuchowski13@gmail.com>
2024-01-03 20:22:47 +0100

    Zapytania asynchroniczne!!!!

--- src/main/java/pl/edu/pw/mini/moneyxchange/ExchangeRateScreen.java
+++ src/main/java/pl/edu/pw/mini/moneyxchange/ExchangeRateScreen.java
@@ -1,68 +1,62 @@
 package pl.edu.pw.mini.moneyxchange;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.javamoney.moneta.FastMoney;
-import org.javamoney.moneta.Money;
-import org.knowm.xchart.*;
-import org.knowm.xchart.internal.chartpart.Chart;
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
 import pl.edu.pw.mini.moneyxchange.nbp.Currency;
 import pl.edu.pw.mini.moneyxchange.nbp.ExchangeRate;
 import pl.edu.pw.mini.moneyxchange.nbp.NBP_API;
 
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.*;
 
 public class ExchangeRateScreen extends JPanel {
 
-    private List<ExchangeRate> exchangeData;
+	private List<ExchangeRate> exchangeData;
+	private JPanel chartPanel;
+	private XYChart chart;
 
-    private JPanel chartPanel;
+	public ExchangeRateScreen() {
+		NBP_API.getCurrencyExchangeRate(Currency.CAD, 255)
+				.thenAccept(data -> {
+					exchangeData = data;
 
-    public ExchangeRateScreen() {
-        exchangeData = NBP_API.getCurrencyExchangeRate(Currency.CAD, 255); // Initialize with dummy data
+					updateChart();
+					repaint();
+				});
 
-        setLayout(new BorderLayout());
-        setBorder(new EmptyBorder(10, 10, 10, 10));
+		setLayout(new BorderLayout());
+		setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        // Create XChart
-        Chart chart = createChart();
-        chartPanel = new XChartPanel<>(chart);
+		chart = new XYChartBuilder()
+				.width(800).height(600)
+				.title("Kurs")
+				.xAxisTitle("Data")
+				.yAxisTitle("Średni kurs")
+				.build();
 
-        // Add components to the main panel
-        add(chartPanel, BorderLayout.CENTER);
-    }
+		chart.getStyler().setLegendVisible(false);
+		chart.getStyler().setToolTipsEnabled(true);
+		chart.getStyler().setDatePattern("yyyy-MM-dd");
 
-    private Chart createChart() {
-        XYChart chart = new XYChartBuilder()
-                .width(800).height(600)
-                .title("Kurs")
-                .xAxisTitle("Data")
-                .yAxisTitle("Średni kurs")
-                .build();
-        chart.getStyler().setLegendVisible(false);
+		chartPanel = new XChartPanel<>(chart);
+		add(chartPanel, BorderLayout.CENTER);
+	}
 
-        List<Date> xAxis = new ArrayList<>();
-        List<Double> yAxis = new ArrayList<>();
+	private void updateChart() {
+		List<Date> xAxis = new ArrayList<>();
+		List<Double> yAxis = new ArrayList<>();
 
-        exchangeData.forEach(rate -> {
-            xAxis.add(rate.date);
-            yAxis.add(rate.value.getNumber().doubleValue());
-        });
+		exchangeData.forEach(rate -> {
+			xAxis.add(rate.date);
+			yAxis.add(rate.value.getNumber().doubleValue());
+		});
 
-        chart.addSeries("EUR", xAxis, yAxis);
-
-        return chart;
-    }
+		chart.removeSeries("Rate");
+		chart.addSeries("Rate", xAxis, yAxis);
+	}
 }
--- src/main/java/pl/edu/pw/mini/moneyxchange/nbp/NBP_API.java
+++ src/main/java/pl/edu/pw/mini/moneyxchange/nbp/NBP_API.java
@@ -1,77 +1,65 @@
 package pl.edu.pw.mini.moneyxchange.nbp;
 
-import com.formdev.flatlaf.json.Json;
-import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.javamoney.moneta.Money;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class NBP_API {
     private static final String EXCHANGE_RATE_ENDPOINT = "https://api.nbp.pl/api/exchangerates/rates/";
     private static final SimpleDateFormat JsonDate = new SimpleDateFormat("yyyy-MM-dd");
     public static final CurrencyUnit PLN = Monetary.getCurrency("PLN");
 
-    private static JsonObject getApiResponse(Currency currency, int count) {
-        JsonObject ret = null;
-        try {
-            URL url = new URL(EXCHANGE_RATE_ENDPOINT + "A/" + currency.toString() + "/last/" + count + "/");
-            System.out.println(url);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("Accept", "application/json");
-            connection.setRequestMethod("GET");
+    private static CompletableFuture<JsonObject> getApiResponse(Currency currency, int count) {
+        HttpClient client = HttpClient.newHttpClient();
 
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(EXCHANGE_RATE_ENDPOINT + "A/" + currency.toString() + "/last/" + count + "/"))
+                .timeout(Duration.ofMinutes(1))
+                .header("Accept", "application/json")
+                .build();
 
-                ret = JsonParser.parseReader(reader).getAsJsonObject();
-
-                reader.close();
-            } else {
-                System.err.println("HTTP GET request failed: " + responseCode);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return ret;
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofInputStream())
+//                .thenApply(inputStreamHttpResponse -> {
+//                    System.out.println(inputStreamHttpResponse.statusCode());
+//                    return inputStreamHttpResponse;
+//                })
+                .thenApply(HttpResponse::body)
+                .thenApply(InputStreamReader::new)
+                .thenApply(JsonParser::parseReader)
+                .thenApply(JsonElement::getAsJsonObject);
     }
 
-    public static List<ExchangeRate> getCurrencyExchangeRate(Currency currency, int count) {
-        JsonObject response = getApiResponse(currency, count);
-
-        List<ExchangeRate> exchangeData = new ArrayList<>();
-
-        JsonArray rates = response.getAsJsonArray("rates");
-        for(var r : rates) {
-            JsonObject rate = (JsonObject) r;
-            Date date;
-            Money money;
-            try {
-                date = JsonDate.parse(rate.get("effectiveDate").getAsString());
-                money = Money.of(rate.get("mid").getAsBigDecimal(), PLN);
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-
-            exchangeData.add(new ExchangeRate(date, money));
-        }
-
-        return exchangeData;
+    public static CompletableFuture<List<ExchangeRate>> getCurrencyExchangeRate(Currency currency, int count) {
+        return getApiResponse(currency, count)
+                .thenApply(response -> response.getAsJsonArray("rates"))
+                .thenApply(rates -> StreamSupport.stream(rates.spliterator(), true)
+                            .map(JsonElement::getAsJsonObject)
+                            .map(rate -> {
+                                try {
+                                    Date date = JsonDate.parse(rate.get("effectiveDate").getAsString());
+                                    Money money = Money.of(rate.get("mid").getAsBigDecimal(), PLN);
+                                    return new ExchangeRate(date, money);
+                                } catch (ParseException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            })
+                            .collect(Collectors.toList()));
     }
 }
