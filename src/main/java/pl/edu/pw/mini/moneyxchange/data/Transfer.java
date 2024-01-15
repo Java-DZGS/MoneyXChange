f7be0c56282693ea729f79816e4768aacc9bb7c6
GitQueenZofia <zofia.anna.grabowska@gmail.com>
2024-01-15 18:40:45 +0100

    Rozdział przelewów

--- src/main/java/pl/edu/pw/mini/moneyxchange/data/Transfer.java
+++ src/main/java/pl/edu/pw/mini/moneyxchange/data/Transfer.java
@@ -1,6 +1,8 @@
 package pl.edu.pw.mini.moneyxchange.data;
 
 import org.javamoney.moneta.Money;
+import pl.edu.pw.mini.moneyxchange.dialogs.CompleteTransferDialog;
+import pl.edu.pw.mini.moneyxchange.dialogs.ExpenseDialog;
 import pl.edu.pw.mini.moneyxchange.utils.Format;
 
 import javax.swing.*;
@@ -57,6 +59,39 @@ public class Transfer implements MoneyAction, Serializable {
         transferPanel.add(titleLabel);
         transferPanel.add(dateLabel);
         transferPanel.add(amountLabel);
+
+        return transferPanel;
+    }
+    public JPanel getOptimalPanel() {
+        JPanel transferPanel = new JPanel(new GridBagLayout());
+        transferPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST; // Ustawienie anchor na WEST wyrówna elementy do lewej
+
+        JLabel usersLabel = new JLabel("Przelew od " + fromUser.getName() + " do " + toUser.getName());
+        JLabel titleLabel = new JLabel("Tytuł: " + getTitle());
+        JLabel amountLabel = new JLabel("Kwota: " + Format.MONETARY_FORMAT.format(amount));
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        transferPanel.add(usersLabel, constraints);
+
+        constraints.gridy = 1;
+        transferPanel.add(titleLabel, constraints);
+
+        constraints.gridy = 2;
+        transferPanel.add(amountLabel, constraints);
+
+        JButton completeButton = new JButton("Zrób przelew");
+        constraints.anchor = GridBagConstraints.EAST; // Ustawienie anchor na EAST wyrówna przycisk do prawej
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        transferPanel.add(completeButton, constraints);
+
+        completeButton.addActionListener(e -> {
+            CompleteTransferDialog completeTransferDialog = new CompleteTransferDialog(Group.getInstance(), this);
+        });
 
         return transferPanel;
     }
---
+++ src/main/java/pl/edu/pw/mini/moneyxchange/dialogs/CompleteTransferDialog.java
@@ -0,0 +1,62 @@
+package pl.edu.pw.mini.moneyxchange.dialogs;
+
+import org.javamoney.moneta.Money;
+import pl.edu.pw.mini.moneyxchange.data.Division;
+import pl.edu.pw.mini.moneyxchange.data.Group;
+import pl.edu.pw.mini.moneyxchange.data.Transfer;
+import pl.edu.pw.mini.moneyxchange.data.User;
+import pl.edu.pw.mini.moneyxchange.utils.Format;
+
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class CompleteTransferDialog extends JDialog {
+    private final JTextField titleField;
+    private final JTextField dateField;
+    private final JTextField amountField;
+    private final Group group;
+    private Money amount;
+    public CompleteTransferDialog(Group group, Transfer transfer) {
+        super((JFrame) null, "Wykonaj przelew", true);
+
+        this.group = group;
+
+        Date today = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = dateFormat.format(today);
+
+
+        titleField = new JTextField(transfer.getTitle());
+        titleField.setEditable(false);
+        dateField = new JTextField(formattedDate);
+        amountField = new JTextField(Format.MONETARY_FORMAT.format(transfer.getAmount()));
+
+        JButton completeButton = new JButton("Wykonaj przelew");
+
+        JPanel panel = new JPanel(new GridLayout(4, 2, 10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add padding
+
+        panel.add(new JLabel("Tytuł:"));
+        panel.add(titleField);
+        panel.add(new JLabel("Data:"));
+        panel.add(dateField);
+        panel.add(new JLabel("Kwota:"));
+        panel.add(amountField);
+        panel.add(completeButton);
+
+        add(panel);
+
+        completeButton.addActionListener(e -> {
+            group.markTransferAsCompleted(transfer);
+            dispose();
+        });
+
+        this.setSize(300, 200);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+}
